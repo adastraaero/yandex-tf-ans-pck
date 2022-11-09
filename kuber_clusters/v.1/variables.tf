e147178a168bb120e30ce08fa22e5f7c1febb808
Dmitry <artofsky@mail.ru>
2022-11-09 03:58:20 -0800

    first-commit

---
+++ kuber_clusters/v.1/variables.tf
@@ -0,0 +1,39 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "key.json"
+}
+variable count_instance {
+  # кол-во создаваемых инстансов
+  default = "2"
+}
+variable network_id {
+  description = "Network id"
+}
+variable service_account_id {
+  description = "Service account ID"
+}
+variable cores {
+  description = "nodes cores"
+  default     = 4
+}
+variable memory {
+  description = "nodes memories"
+  default     = 8
+}
+variable size {
+  description = "nodes sizes"
+  default     = 64
+}
---
+++ simple_virtuals/v.1/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+
+provider "yandex" {
+  token     = "AQAAAAAAjZ8hAATuwZffBhgv_UDVtOb-lDno6-4" # *OAuth-токен яндекса*
+	# не обязательный параметр (берется облако по умолчанию),
+	# хотя в документации написано иначе
+  cloud_id  = "b1gtiaf7phq3prgm1q7i"
+  folder_id = "b1gl9g5f46b3fv1g4ac1"
+  zone      = "ru-central1-a"
+}
+
+data "yandex_compute_image" "last_ubuntu" {
+  family = "ubuntu-2204-lts"  # ОС (Ubuntu, 22.04 LTS)
+}
+
+data "yandex_vpc_subnet" "default_a" {
+  name = "default-ru-central1-a"  # одна из дефолтных подсетей
+}
+
+
+
+
+# ресурс "yandex_compute_instance" т.е. сервер
+# Terraform будет знаеть его по имени "yandex_compute_instance.default"
+resource "yandex_compute_instance" "default" { 
+  name = "test-instance"
+	platform_id = "standard-v1" # тип процессора (Intel Broadwell)
+
+  resources {
+    core_fraction = 5 # Гарантированная доля vCPU
+    cores  = 2 # vCPU
+    memory = 1 # RAM
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.last_ubuntu.id
+    }
+  }
+
+  network_interface {
+    subnet_id = data.yandex_vpc_subnet.default_a.subnet_id
+    nat = true # автоматически установить динамический ip
+  }
+}
+
